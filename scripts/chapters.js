568e9a904c66398636ec912dad33442cfc3d7298
7oi <johann@mms.is>
2016-09-02 11:02:34 +0000

    Adds data structure examples and changes .gitignore to include them

---
+++ scripts/chapters.js
@@ -0,0 +1,672 @@
+/* jshint unused: false */
+var data = {
+	"main-title": "Á ferð um samfélagið",
+	"vnr": "MMS0000",
+	"headerImg": "images/main/banner.png",
+	"footerImg": "https://mms.is/sites/mms.is/themes/mms_theme/assets/images/menta-logo.png",
+	"backgroundImg": "/images/main/background@0,5x.png",
+	"chapters": [
+	{
+		"chapter": "about",
+		"title": "Til kennarans",
+		"thumb": "images/chapters/about/thumb.png",
+		"banner": "images/chapters/about/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "TIL KENNARA",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "MARKMIÐ",
+				"contents": ""
+			}
+		]
+	},
+	{
+		"chapter": "k1",
+		"title": "Hvað er samfélag?",
+		"thumb": "images/chapters/k1/thumb.png",
+		"banner": "images/chapters/k1/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": `
+					<h1>Hello world</h1>
+
+					<p>This is dog</p>
+					<p><small>Cat was not invited</small></p>
+				`
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k2",
+		"title": "Sinn er siður",
+		"thumb": "images/chapters/k2/thumb.png",
+		"banner": "images/chapters/k2/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k3",
+		"title": "Félagsmótun",
+		"thumb": "images/chapters/k3/thumb.png",
+		"banner": "images/chapters/k3/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k4",
+		"title": "Menning og samfélag",
+		"thumb": "images/chapters/k4/thumb.png",
+		"banner": "images/chapters/k4/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k5",
+		"title": "Fjölskyldan",
+		"thumb": "images/chapters/k5/thumb.png",
+		"banner": "images/chapters/k5/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k6",
+		"title": "Gaman saman",
+		"thumb": "images/chapters/k6/thumb.png",
+		"banner": "images/chapters/k6/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k7",
+		"title": "Vinna og framleiðsla",
+		"thumb": "images/chapters/k7/thumb.png",
+		"banner": "images/chapters/k7/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k8",
+		"title": "Stjórnmál",
+		"thumb": "images/chapters/k8/thumb.png",
+		"banner": "images/chapters/k8/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k9",
+		"title": "Trúarbrögð",
+		"thumb": "images/chapters/k9/thumb.png",
+		"banner": "images/chapters/k9/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k10",
+		"title": "Viðmið og frávik",
+		"thumb": "images/chapters/k10/thumb.png",
+		"banner": "images/chapters/k10/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	},
+	{
+		"chapter": "k11",
+		"title": "Alþjóðasamfélag og mannaréttindi",
+		"thumb": "images/chapters/k11/thumb.png",
+		"banner": "images/chapters/k11/banner.png",
+		"parts" : [
+			{
+				"id": "p1",
+				"type": "tab",
+				"title": "MARKMIÐ KAFLANS",
+				"contents": "<h1>Hello world</h1>"
+			},
+			{
+				"id": "p2",
+				"type": "tab",
+				"title": "KVEIKJUR",
+				"contents": ""
+			},
+			{
+				"id": "p3",
+				"type": "tab",
+				"title": "LJÓSMYNDAMARAÞON",
+				"contents": ""
+			},
+			{
+				"id": "p4",
+				"type": "tab",
+				"title": "KRÆKJUR OG ÍTAREFNI",
+				"contents": ""
+			},
+			{
+				"id": "p5",
+				"type": "dropdown",
+				"title": "VERKEFNI",
+				"contents": "",
+				"items": [
+					{
+						"id": "dp1",
+						"type": "item",
+						"title": "Útfærsla á verkefnum",
+						"contents": "<h1>Alls konar eitthvað</h1>"
+					},
+					{
+						"id": "dp2",
+						"type": "item",
+						"title": "VIÐBÓTARVERKEFNI",
+						"contents": ""
+					},
+					{
+						"id": "dp3",
+						"type": "item",
+						"title": "VERKEFNABLÖÐ",
+						"contents": ""
+					},
+				]
+			}
+		]
+	}
+]};
